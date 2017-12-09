a153b041cf2988d95fc122b6c5dfc3aa101d2634
nac0n <lager_91@hotmail.com>
2017-12-09 17:50:12 +0100

    initial commit, added MenuActivity

--- app/src/main/java/kanhoudevelopment/waifurun/MenuActivity.java
+++ app/src/main/java/kanhoudevelopment/waifurun/MenuActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class GameActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
